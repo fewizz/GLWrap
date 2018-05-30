4cae7c855e3c453f06e7ffeb3b52137e50025f4b
fewizz <hazeevaidar@gmail.com>
2018-05-31 00:32:26 +0300

    Generate some methods

--- src/main/java/fewizz/wrap/gl/GLObjectWithId.java
+++ src/main/java/fewizz/wrap/gl/GLObjectWithId.java
@@ -1,5 +1,7 @@
 package fewizz.wrap.gl;
 
+import java.util.Objects;
+
 public abstract class GLObjectWithId<SELF> {
 	String label;
 	final GLContext ctx;
@@ -36,5 +38,19 @@ public abstract class GLObjectWithId<SELF> {
 	@Override
 	public String toString() {
 		return "[" + getClass().getSimpleName() + " id:" +  name + "]";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		GLObjectWithId<?> that = (GLObjectWithId<?>) o;
+		return name == that.name &&
+				Objects.equals(ctx, that.ctx);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ctx, name);
 	}
 }
